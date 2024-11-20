02b75075b40a82330f2231c87e72c11b0b82960c
arefjami <amirmp2332@gmail.com>
2024-11-20 13:15:46 +0330

    front is fix admin panel is neded

--- app/V/admin/categories.php
+++ app/V/admin/categories.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0;">
-<title>قالب پنل مدیریت |نت کپی</title>    <link rel="stylesheet" href="css/style.css">
+<title>قالب پنل مدیریت |نت کپی</title>    <link rel="stylesheet" href="wive/style.css">
     <link rel="stylesheet" href="wive/index/css/responsive_991.css" media="(max-width:991px)">
     <link rel="stylesheet" href="wive/index/css/responsive_768.css" media="(max-width:768px)">
     <link rel="stylesheet" href="wive/index/css/font.css">
